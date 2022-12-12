bf000d2856993bd1b58edd76b08c9ae95f5d9281
eliyahukoren <elik.koren@gmail.com>
2022-12-12 14:23:03 +0200

    local-api: use proxy or local

--- packages/local-api/dist/index.js
+++ packages/local-api/dist/index.js
@@ -4,11 +4,24 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.serve = void 0;
+var path_1 = __importDefault(require("path"));
 var express_1 = __importDefault(require("express"));
-var serve = function (port, filename, dir) {
+var http_proxy_middleware_1 = require("http-proxy-middleware");
+var serve = function (port, filename, dir, useProxy) {
     var app = (0, express_1.default)();
+    if (useProxy) {
+        app.use((0, http_proxy_middleware_1.createProxyMiddleware)({
+            target: 'http://localhost:3000',
+            ws: true,
+            logLevel: 'silent'
+        }));
+    }
+    else {
+        var packagePath = require.resolve("local-client/build/index.html");
+        app.use(express_1.default.static(path_1.default.dirname(packagePath)));
+    }
     return new Promise(function (resolve, reject) {
-        app.listen(port, resolve).on('error', reject);
+        app.listen(port, resolve).on("error", reject);
     });
 };
 exports.serve = serve;
--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,12 +1,27 @@
-import express from 'express';
+import path from "path";
+import express from "express";
+import { createProxyMiddleware } from "http-proxy-middleware";
 
-export const serve = (port: number, filename: string, dir: string) => {
+export const serve = (
+	port: number,
+	filename: string,
+	dir: string,
+	useProxy: boolean
+) => {
 	const app = express();
 
+	if(useProxy){
+		app.use(createProxyMiddleware({
+			target: 'http://localhost:3000',
+			ws: true,
+			logLevel: 'silent'
+		}));
+	}else{
+		const packagePath = require.resolve("local-client/build/index.html");
+		app.use(express.static(path.dirname(packagePath)));
+	}
+
 	return new Promise<void>((resolve, reject) => {
-		app.listen(port, resolve).on('error', reject);
+		app.listen(port, resolve).on("error", reject);
 	});
-
 };
-
-
