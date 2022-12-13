f5960051638fb5bd1822fc1923221b1b1e0f2771
eliyahukoren <elik.koren@gmail.com>
2022-12-13 10:38:00 +0200

    prepare for npm publish

--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -19,7 +19,7 @@ export const serve = (
 			logLevel: 'silent'
 		}));
 	}else{
-		const packagePath = require.resolve("local-client/build/index.html");
+		const packagePath = require.resolve("@jsnote-ek/local-client/build/index.html");
 		app.use(express.static(path.dirname(packagePath)));
 	}
 
